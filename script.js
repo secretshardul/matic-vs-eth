572cbeda0bb227268499c8bb88eb136014a8f306
shardul <shardul.aeer@somaiya.edu>
2021-04-01 00:24:46 +0530

    feat: Display USD gas costs

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let maticFees = undefined
+let ethFees = undefined
+
 async function getMaticFees() {
     const resp = await fetch('https://gasstation-mainnet.matic.network')
     const fees = await resp.json()
@@ -14,13 +17,7 @@ async function getEthFees() {
     return fees
 }
 
-google.charts.load('current', { packages: ['corechart', 'bar'] })
-google.charts.setOnLoadCallback(drawCharts)
-
 async function drawCharts() {
-    const maticFees = await getMaticFees()
-    const ethFees = await getEthFees()
-
     const values = [
         ['Speed', 'Matic', 'Ethereum'],
         ['Slow', maticFees.safeLow, ethFees.safeLow / 10],
@@ -40,3 +37,81 @@ function draw(values, labelName, elementId) {
     const materialChart = new google.charts.Bar(document.getElementById(elementId))
     materialChart.draw(data, materialOptions)
 }
+
+function getTable (mFast, mMed, mSlow, ethFast, ethMed, ethSlow) {
+    return `<table class="table">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Fast 🚀</th>
+                        <th>Medium 🚗</th>
+                        <th>Slow 🐌</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <div id="matic-prices"></div>
+                        <td>Matic</td>
+                        <td>${mFast}</td>
+                        <td>${mMed}</td>
+                        <td>${mSlow}</td>
+                    </tr>
+                    <tr>
+                        <div id="matic-prices"></div>
+                        <td>Ethereum</td>
+                        <td>${ethFast}</td>
+                        <td>${ethMed}</td>
+                        <td>${ethSlow}</td>
+                    </tr>
+
+                </tbody>
+            </table>`
+}
+
+function getFee(gasPrice, rate) {
+    const gasUsed = 21000
+    const cost = gasPrice * gasUsed * 0.000000001 * rate
+    // const cost = (gwei * rate) / 10 ** 9
+    return cost.toFixed(6)
+}
+
+async function getExchangeRates() {
+    const resp = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum%2Cmatic-network&vs_currencies=usd')
+    const rates = await resp.json()
+
+}
+
+
+async function getGasFees() {
+    maticFees = await getMaticFees()
+    ethFees = await getEthFees()
+
+    // Draw column graph
+    google.charts.load('current', { packages: ['corechart', 'bar'] })
+    google.charts.setOnLoadCallback(drawCharts)
+
+    const gasTable = document.getElementById('gas-table')
+
+
+    // Get exchange rates
+    const resp = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum%2Cmatic-network&vs_currencies=usd')
+    const rates = await resp.json()
+
+    const maticRate = rates['matic-network'].usd
+    const ethRate = rates.ethereum.usd
+
+    // Calculate fees and generate table
+    const table = getTable(
+        getFee(maticFees.fast, maticRate),
+        getFee(maticFees.standard, maticRate),
+        getFee(maticFees.safeLow, maticRate),
+
+        getFee(ethFees.fast / 10, ethRate),
+        getFee(ethFees.average / 10, ethRate),
+        getFee(ethFees.safeLow / 10, ethRate),
+    )
+
+    // Display table
+    gasTable.innerHTML = table
+}
+
